95a3031662182cb6f280b386d157fa3e5f05cff6
Kerlyson <kerlyson_sk@yahoo.com.br>
2020-01-30 20:06:12 -0300

    rota e navegação para formulario aluno

--- tcc-projeto-angular/src/app/cadastro/cadastro-routing.module.ts
+++ tcc-projeto-angular/src/app/cadastro/cadastro-routing.module.ts
@@ -1,11 +1,17 @@
-import { CadastroProfessorComponent } from './pages/cadastro-professor/cadastro-professor.component';
 import { Routes, RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
+
+import { CadastroProfessorComponent } from './pages/cadastro-professor/cadastro-professor.component';
+import { CadastroAlunoComponent } from './pages/cadastro-aluno/cadastro-aluno.component';
 
 const routes: Routes = [
   {
     path: 'professor',
     component: CadastroProfessorComponent,
+  },
+  {
+    path: 'aluno',
+    component: CadastroAlunoComponent,
   },
 ];
 
